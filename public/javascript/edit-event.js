ee8b8c0735cce4b1ae199e7c1f329f350d9a00a9
Travis Lloyd <travis.t.lloyd1996@gmail.com>
2022-01-01 20:57:30 -0700

    working on edit events

--- public/javascript/edit-event.js
+++ public/javascript/edit-event.js
@@ -1,14 +1,20 @@
 async function editFormHandler(event) {
     event.preventDefault();
   
-    const title = document.querySelector('input[name="event-title"]').value.trim();
+    const event_name = document.querySelector('input[name="event-title"]').value.trim();
+    const date = document.querySelector('input[name="event-date"]').value;
+    const description = document.querySelector('input[name="event-description"]').value;
+    const planner_name=;
+    const planner_contact=;
     const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
     const response = await fetch(`/api/events/${id}`, {
       method: 'PUT',
       body: JSON.stringify({
-        title
+        event_name,
+        date,
+        description
       }),
       headers: {
         'Content-Type': 'application/json'
@@ -21,5 +27,11 @@ async function editFormHandler(event) {
       alert(response.statusText);
     }
   }
+  'id',
+  'event_name',
+  'date',
+  'description',
+  'planner_name',
+  'planner_contact'
   
   document.querySelector('.edit-event-form').addEventListener('submit', editFormHandler);
